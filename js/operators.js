868318b8f35daad20ce97b3df085619f631765b9
kristian-wittrup <kw@easv.dk>
2016-02-17 13:52:13 +0100

    operators + new files

---
+++ js/operators.js
@@ -0,0 +1,43 @@
+var x = 5;
+var y = 3;
+var z = 2;
+
+// alert (x / y);  <= equals 1
+//alert (x % y);   <= equals 0.
+
+
+//example with the modulus operator % - fx if you page have many paragraphs and can be used well with the "if" statement when you want target every 3 item/row and make changes to them only.
+
+
+//using the += operator
+
+z += 2; // is the same as z = z + 2;
+
+/* try using some of the other operators to do math :)
+ fx this one:
+ var h = 3;
+ ++h;  // is the same as h = 3 + 1; in this case. Where could this be used to great effect?
+*/
+
+// alert(z);
+
+
+// Relational operator test. == (is it the same?)
+//alert(x !== y);
+
+
+//Using conditional statements  (only do a alertbox IF x is greater then y
+//if(x > y)
+//    alert("test");
+
+// Conditional + CONJUNCTION
+//      1    1      1     <= look at our table with expr and results
+// if(x > y && y == 3)
+// alert("test");
+
+
+
+//DISJUNCTION
+//      0    1      1     <= Check the disjunction table. Try to remove the alert box by changing our code.
+// if(x < y || y == 3)
+//    alert("test");
